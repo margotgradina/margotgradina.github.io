dac92b1f6df4a99c41690ff4ac4b51ecf566b476
Margot <mdehaas@msn.com>
2023-10-26 01:05:26 +0200

    css

--- my-app/src/components/pages/contact/ThankYou.tsx
+++ my-app/src/components/pages/contact/ThankYou.tsx
@@ -5,8 +5,11 @@ const ThankYou = () => {
     <div
       className={css`
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
+        justify-content: center;
         align-items: center;
+        width: 100%;
+        height: 100%;
       `}
     >
       <p>Thank you for your message. I will get back to you as soon as possible!</p>
