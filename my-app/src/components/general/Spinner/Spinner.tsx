c9e44828ec9ba971ae2b920c9772e172acd8055f
Margot <mdehaas@msn.com>
2023-10-25 22:28:01 +0200

    Add spinner

---
+++ my-app/src/components/general/Spinner/Spinner.tsx
@@ -0,0 +1,35 @@
+import "./Spinner.css";
+
+const SpinnerComponent = () => {
+  return (
+    <>
+      <div className={"spinner-container"}>
+        <div className={"spinner-element"}>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SpinnerComponent;
