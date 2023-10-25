a861c5a86b3e296e599f5945770041bda0259b67
Margot <mdehaas@msn.com>
2023-10-26 00:46:39 +0200

    projects page

--- my-app/src/components/pages/projects/Projects.tsx
+++ my-app/src/components/pages/projects/Projects.tsx
@@ -54,9 +54,9 @@ const Projects = () => {
         className={css`
           display: flex;
           width: 100%;
-          height: 100%;
-
+          height: 97%;
           align-items: center;
+          align-self: flex-end;
           justify-content: center;
           flex-direction: row;
           gap: 1rem;
@@ -74,46 +74,108 @@ const Projects = () => {
             </div>
             <img
               className={css`
-                width: auto;
-                object-fit: cover;
+                width: 100%;
                 max-width: 30%;
+                object-fit: cover;
+                object-position: top left;
                 height: 40vh;
-                max-height: 400px;
+                min-height: 200px;
+                /* max-height: 400px; */
                 border-radius: 15px;
               `}
               src={projects[currentProject].image}
-            ></img>
+            />
             <div
               className={css`
+                display: flex;
+                flex-direction: column;
+                overflow: hidden;
                 width: 30%;
                 height: 40vh;
-                max-height: 400px;
+                /* max-height: 400px; */
+                min-height: 200px;
                 border-radius: 15px;
                 padding: 0px;
-                overflow-y: auto;
                 background-color: rgba(124, 192, 160, 0.3);
-                ::-webkit-scrollbar {
-                  width: 1px; /* width of the entire scrollbar */
-                }
-
-                ::-webkit-scrollbar-thumb {
-                  background-color: #7cc0a0; /* color of the scroll thumb */
-                  border-radius: 20px; /* roundness of the scroll thumb */
-                  border: 0px solid orange; /* creates padding around scroll thumb */
-                }
               `}
             >
-              <p
+              <div
                 className={css`
-                  padding: 5px;
+                  height: max-content;
                   font-family: "Raleway";
-                  font-weight: 400;
-                  text-align: justify;
-                  font-size: small;
+                  font-weight: 500;
+                  font-size: calc(0.7rem + 0.5vw);
+                  padding-left: 10px;
+                  padding-top: 10px;
                 `}
               >
-                {projects[currentProject].text}
-              </p>
+                {projects[currentProject].title}
+              </div>
+              <div
+                className={css`
+                  flex: 1;
+                  overflow-y: auto;
+
+                  ::-webkit-scrollbar {
+                    width: 2px; /* width of the entire scrollbar */
+                  }
+
+                  ::-webkit-scrollbar-thumb {
+                    background-color: #7cc0a0; /* color of the scroll thumb */
+                    border-radius: 20px; /* roundness of the scroll thumb */
+                    border: 0px solid orange; /* creates padding around scroll thumb */
+                  }
+                `}
+              >
+                <div
+                  className={css`
+                    padding: 15px;
+                    font-family: "Raleway";
+                    font-weight: 400;
+                    font-size: calc(0.5rem + 0.5vw);
+                    text-align: justify;
+                    white-space: pre-wrap;
+                  `}
+                >
+                  <p>{projects[currentProject].text}</p>
+                </div>
+              </div>
+              <div
+                className={css`
+                  display: flex;
+                  height: max-content;
+
+                  flex-direction: row;
+                  flex-wrap: wrap;
+                  gap: 5px;
+                  padding: 10px;
+                `}
+              >
+                {projects[currentProject].tools.map((tool) => {
+                  return (
+                    <>
+                      {" "}
+                      <div
+                        className={css`
+                          background-color: rgba(67, 151, 112, 0.24);
+                          border-radius: 10px;
+                          height: max-content;
+                          width: max-content;
+                          padding: 7px;
+                          font-size: calc(0.4rem + 0.3vw);
+                          transition: opacity 4s;
+                          :hover {
+                            opacity: 0.2;
+                            transition: opacity 0.5s;
+                          }
+                        `}
+                      >
+                        {tool}
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
             </div>
             <div
               onClick={() => handleNext()}
